32087a89c5139082528471f594a7bed3bac410eb
David Moreno García <david.mogar@gmail.com>
2015-05-07 11:20:21 +0200

    Add facepp support and image cache

--- lexgen.py
+++ lexgen.py
@@ -1,11 +1,12 @@
 import codecs
 import csv
 import datetime
+import urllib
+import facepp
+import genderator
 import json
 import operator
 import os
-
-import genderator
 
 
 MINIMAL_CONFIDENCE = 0.75
@@ -26,6 +27,31 @@ tweets_classified = 0
 female_users = {}
 male_users = {}
 
+facepp_cache = {}
+
+api = facepp.API('14593ac430ff440a9fdc92e361efea71', 'RPzUPKrotSUztSW8QRk0SfIT5CPbXX5C', 'http://api.us.faceplusplus.com/')
+
+
+def load_facepp_cache():
+    print('Loading Face++ cache data')
+
+    try:
+        with codecs.open(os.path.join(path, 'data/facepp_cache.tsv'), 'r', 'UTF-8') as file:
+            for line in file:
+                (url, json_data) = line.split('\t')
+                facepp_cache[url] = json.loads(json_data)
+        file.close()
+    except IOError:
+        print('Cache file not found')
+
+def update_facepp_cache():
+    print('\nUpdating Face++ cache file\n')
+
+    with codecs.open(os.path.join(path, 'data/top_females.tsv'), 'w', 'UTF-8') as file:
+        writer = csv.writer(file, delimiter='\t')
+        for (screen_name, count) in sorted(female_users.items(), key=operator.itemgetter(1), reverse=True):
+            writer.writerow([screen_name, count])
+    file.close()
 
 def classify_tweet(user_screen_name, text, gender, confidence):
     global female_tweets, male_tweets, tweets_classified
@@ -75,14 +101,33 @@ def classify_tweets():
 
             prediction = guesser.guess_gender(user_name)
 
-            if prediction is not None and prediction['surnames']:
+            if prediction is not None:
                 confidence = prediction['confidence']
                 gender = prediction['gender']
+
+                if confidence < MINIMAL_CONFIDENCE or not prediction['surnames']:
+                    profile_image = tweet['user']['profile_image_url'].replace('_normal', '')
+                    api_error = False
+                    if profile_image in facepp_cache:
+                        answer = facepp_cache[profile_image]
+                    else:
+                        try:
+                            answer = api.detection.detect(url=profile_image)
+                        except:
+                            api_error = True
+
+                    if not api_error and len(answer['face']) == 1:
+                        facepp_cache[profile_image] = answer
+                        temp_gender = answer['face'][0]['attribute']['gender']['value']
+                        temp_confidence = answer['face'][0]['attribute']['gender']['confidence']
+                        if gender == temp_gender and temp_confidence >= MINIMAL_CONFIDENCE:
+                            gender = temp_gender
+                            confidence = temp_confidence
 
                 if confidence >= MINIMAL_CONFIDENCE:
                     classify_tweet(user_screen_name, tweet['text'], gender, confidence)
 
-            if tweets_processed > 1 and tweets_processed % 50000 == 0:
+            if tweets_processed > 1 and tweets_processed % 100 == 0:
                 millis_elapsed += (datetime.datetime.now() - time).total_seconds() * 1000
                 millis_to_finish = (millis_elapsed / tweets_processed) * (total_tweets - tweets_processed)
                 time = datetime.datetime.now()
@@ -110,8 +155,10 @@ def generate_top_files():
     file.close()
 
 
+load_facepp_cache()
 classify_tweets()
 generate_top_files()
+update_facepp_cache()
 
 print(tweets_classified, 'tweets classified')
 print(female_tweets, 'tweets from females')
