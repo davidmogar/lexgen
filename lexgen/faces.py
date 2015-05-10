dad5a0c840a9c4aa29e898816be64dbe96232335
David Moreno García <david.mogar@gmail.com>
2015-05-10 23:06:29 +0200

    Fix facial recognition
    This commit also improve cache saving. Now every time a new request is made, the answer is cached (to dict and file). This is slower, but better in case of errors.

--- lexgen/faces.py
+++ lexgen/faces.py
@@ -1,4 +1,4 @@
-import codecs
+import csv
 import json
 import os
 
@@ -9,9 +9,8 @@ CACHE_FILE = os.path.join(os.path.dirname(__file__), 'data/facepp_cache.tsv')
 
 class Faces:
 
-    cache = {}
-
     def __init__(self, key, secret, server='http://api.us.faceplusplus.com/'):
+        self._cache = {}
         self._load_cache()
         self._api = facepp.API(key, secret, server)
 
@@ -26,18 +25,22 @@ class Faces:
         Returns:
             The gender detected and the prediction confidence.
         """
-        valid_answer = True
+        already_cached = False
 
-        if profile_image in self._faces.cache:
-            answer = self._facepp.cache[profile_image]
+        if profile_image in self._cache:
+            answer = self._cache[profile_image]
+            already_cached = True
         else:
             try:
                 answer = self._api.detection.detect(url=profile_image)
             except facepp.APIError:
-                valid_answer = False
+                answer = {}
 
-        if valid_answer and len(answer['face']) == 1:
-            self.cache[profile_image] = answer
+        self._cache[profile_image] = answer
+        if not already_cached:
+            self._store_answer(profile_image, answer)
+
+        if 'face' in answer and len(answer['face']) == 1:
             gender = answer['face'][0]['attribute']['gender']['value']
             confidence = answer['face'][0]['attribute']['gender']['confidence']
             return gender, confidence
@@ -49,9 +52,14 @@ class Faces:
         Load cached data if it was generated previously.
         """
         try:
-            with codecs.open(CACHE_FILE, 'r', 'UTF-8') as file:
+            with open(CACHE_FILE, 'r', encoding='utf-8') as file:
                 for line in file:
                     url, json_data = line.split('\t')
-                    self.cache[url] = json.loads(json_data)
+                    self._cache[url] = json.loads(json_data.replace('\'', '"'))
         except FileNotFoundError:
             pass
+
+    def _store_answer(self, profile_image, answer):
+        with open(CACHE_FILE, 'a+', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file, delimiter='\t')
+            writer.writerow([profile_image, answer])
