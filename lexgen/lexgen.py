7923a701c3a947bf5323cd87a0a9e5c039f0a272
David Moreno García <david.mogar"gmail.com>
2015-05-10 23:13:30 +0200

    Add default value to help text

--- lexgen/lexgen.py
+++ lexgen/lexgen.py
@@ -4,12 +4,13 @@ import os
 
 from .classifier import ChickSexer
 
+
 BASE_DIR = os.path.dirname(__file__)
 MIN_CONFIDENCE = 0.75
 
 
 def create_results_path(args):
-    path = '../data/' + ('faces' if args.faces else 'no-faces') +\
+    path = '../data/' + ('faces' if args.faces else 'no-faces') + \
            ('-surnames' if args.surnames else 'no-surnames') + '-confidence-' + str(args.confidence)
     path = os.path.join(BASE_DIR, path, str(datetime.datetime.now().timestamp()))
     os.makedirs(path)
@@ -26,7 +27,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Generates a lexicon for gender recognition.')
     parser.add_argument('dataset', help='file with JSON objects to be processed')
     parser.add_argument('--faces', action='store_true', help='apply facial recognition over profile images')
-    parser.add_argument('--confidence', metavar='N', type=float, default=0.75, help="minimal confidence for a valid recognition")
+    parser.add_argument('--confidence', metavar='N', type=float, default=0.75,
+                        help="minimal confidence for a valid recognition (default=0.75)")
     parser.add_argument('--surnames', action='store_true', help='require fullnames (at least one surname)')
     parser.add_argument('--overwrite', action='store_true', help='overwrite results directory if exists')
 
