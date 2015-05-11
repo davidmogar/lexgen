19c578088c03652791aaebf04cd1777f2d8a54ee
David Moreno García <david.mogar@gmail.com>
2015-05-11 20:31:43 +0200

    Add a function to calculate root log-likelihood ratio

--- lexgen/utils.py
+++ lexgen/utils.py
@@ -49,3 +49,36 @@ def filter_dict_by_iqr(dictionary):
             filtered_dict[key] = dictionary[key]
 
     return filtered_dict
+
+
+def rootLogLikelihoodRatio(a, b, c, d):
+    """
+    Calculates the root log-likelihood ratio for two events.
+
+    This implementation is based on LLR interpretation provided in the following
+    paper (see table and equations in page 7):
+
+    Java, Akshay, et al. "Why we twitter: understanding microblogging usage and
+    communities." Proceedings of the 9th WebKDD and 1st SNA-KDD 2007 workshop on
+    Web mining and social network analysis. ACM, 2007.
+    Available at: http://aisl.umbc.edu/resources/369.pdf
+
+    Params:
+        a (int): Frequency of token of interest in dataset A.
+        b (int): Frequency of token of interest in dataset B.
+        c (int): Total number of observations in dataset A.
+        d (int): Total number of observations in dataset B.
+
+    Returns:
+        The root log-likelihood ratio.
+    """
+    e1 = c * (a + b) / (c + d)
+    e2 = d * (a + b) / (c + d)
+
+    # Avoid division zero by replacing a or b by 1 if equal to 0
+    result = math.sqrt(2 * (a * math.log(a / e1 or 1) + b * math.log(b / e2 or 1)))
+
+    if a / c < b / d:
+        result = -result
+
+    return result
