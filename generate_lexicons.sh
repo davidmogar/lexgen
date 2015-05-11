18329d94fc1dde7a940e8dd3e36ccbcc02c95eee
David Moreno García <david.mogar@gmail.com>
2015-05-12 00:11:11 +0200

    Add a Bash script to generate lexicons

---
+++ generate_lexicons.sh
@@ -0,0 +1,195 @@
+#! /bin/bash
+#
+# Script to generate multiple datasets while I'm sleeping ;)
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.5
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.5
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.25
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.25
+
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --faces --surnames --confidence 0.75 --lexicon-percentage 0.05
+
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --faces --surnames --confidence 0.9 --lexicon-percentage 0.05
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --faces  --confidence 0.9 --lexicon-percentage 0.25
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --faces  --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --faces --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.5 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.25 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.75 --lexicon-percentage 0.05 --remove-outliers
+
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
+python3 -m lexgen --surnames --confidence 0.9 --lexicon-percentage 0.05 --remove-outliers
